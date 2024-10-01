3992050895f9e691adc0b9f822add4afcea4c674
aw <awr417h@pm.me>
2024-10-01 19:02:57 +0400

    Fix: Ensure correct Docker build context by setting script directory

--- arm7hf.sh
+++ arm7hf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Change to the directory where this script is located
+cd "$(dirname "$0")" || exit 1
+
 docker build --build-arg ARCH=armv7hf --build-arg CHIP=cpu --tag inference_arm .
 docker cp $(docker create inference_arm):/opt/app ./build
 mv build/*.eap .
--- artpec8.sh
+++ artpec8.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Change to the directory where this script is located
+cd "$(dirname "$0")" || exit 1
+
 docker build --build-arg ARCH=aarch64 --build-arg CHIP=artpec8 --tag inference .
 docker cp $(docker create inference):/opt/app ./build
 mv build/*.eap .
